c9239ed507271900f8772bcb18944d9c61e37f14
Jari Vetoniemi <mailroxas@gmail.com>
2013-11-17 15:35:41 +0200

    [buffer] assert more stuff in test

--- buffer/test.c
+++ buffer/test.c
@@ -11,17 +11,20 @@ int main(void)
    short s;
    char c, *str;
 
+   chckBufferEndianType notnative = CHCK_BUFFER_ENDIAN_BIG;
+   if (chckBufferIsBigEndian()) notnative = CHCK_BUFFER_ENDIAN_LITTLE;
+
    /* TEST: little endian buffer */
    {
       static const char test[] = "\x12this integer is 5:\x5\x0\x0\0\1\0\1";
       chckBuffer *buf = chckBufferNewFromPointer(test, sizeof(test)-1, CHCK_BUFFER_ENDIAN_LITTLE);
-      chckBufferReadString(buf, 1, &str);
-      assert(!strcmp(str, "this integer is 5:"));
-      chckBufferReadInt32(buf, &i);
+      assert(chckBufferReadString(buf, 1, &str) == 1);
+      assert(strcmp(str, "this integer is 5:") == 0); free(str);
+      assert(chckBufferReadInt32(buf, &i) == 1);
       assert(i == 5);
-      chckBufferReadInt16(buf, &s);
+      assert(chckBufferReadInt16(buf, &s) == 1);
       assert(s == 1);
-      chckBufferReadInt8(buf, &c);
+      assert(chckBufferReadInt8(buf, &c) == 1);
       assert(c == 1);
       assert(chckBufferGetOffset(buf) - chckBufferGetSize(buf) == 0);
       chckBufferFree(buf);
@@ -31,13 +34,13 @@ int main(void)
    {
       static const char test[] = "\x12this integer is 5:\x0\x0\0\x5\0\1\1";
       chckBuffer *buf = chckBufferNewFromPointer(test, sizeof(test)-1, CHCK_BUFFER_ENDIAN_BIG);
-      chckBufferReadString(buf, 1, &str);
-      assert(!strcmp(str, "this integer is 5:"));
-      chckBufferReadInt32(buf, &i);
+      assert(chckBufferReadString(buf, 1, &str) == 1);
+      assert(strcmp(str, "this integer is 5:") == 0); free(str);
+      assert(chckBufferReadInt32(buf, &i) == 1);
       assert(i == 5);
-      chckBufferReadInt16(buf, &s);
+      assert(chckBufferReadInt16(buf, &s) == 1);
       assert(s == 1);
-      chckBufferReadInt8(buf, &c);
+      assert(chckBufferReadInt8(buf, &c) == 1);
       assert(c == 1);
       assert(chckBufferGetOffset(buf) - chckBufferGetSize(buf) == 0);
       chckBufferFree(buf);
@@ -49,24 +52,24 @@ int main(void)
       chckBuffer *buf = chckBufferNew(5, CHCK_BUFFER_ENDIAN_NATIVE);
       assert(chckBufferIsNativeEndian(buf) == 1);
 
-      chckBufferWriteString(buf, 4, "test");
-      chckBufferResize(buf, 5+sizeof(int32_t)+sizeof(int16_t));
+      assert(chckBufferWriteString(buf, 4, "test") == 1);
+      assert(chckBufferResize(buf, 5+sizeof(int32_t)+sizeof(int16_t)) == 1);
       assert(chckBufferGetSize(buf) == 5+sizeof(int32_t)+sizeof(int16_t));
       assert(chckBufferGetOffset(buf) == 5);
       assert(chckBufferGetOffsetPointer(buf) != chckBufferGetPointer(buf));
 
-      chckBufferWriteUInt32(buf, i);
-      chckBufferWriteUInt16(buf, s);
+      assert(chckBufferWriteUInt32(buf, i) == 1);
+      assert(chckBufferWriteUInt16(buf, s) == 1);
       assert(chckBufferGetOffset(buf) - chckBufferGetSize(buf) == 0);
 
-      chckBufferSeek(buf, 0, SEEK_SET);
+      assert(chckBufferSeek(buf, 0, SEEK_SET) == chckBufferGetOffset(buf));
       assert(chckBufferGetOffset(buf) == 0);
 
-      chckBufferReadString(buf, 1, &str);
-      assert(!strcmp(str, "test"));
-      chckBufferReadInt32(buf, &i);
+      assert(chckBufferReadString(buf, 1, &str) == 1);
+      assert(strcmp(str, "test") == 0); free(str);
+      assert(chckBufferReadInt32(buf, &i) == 1);
       assert(i == 8);
-      chckBufferReadInt16(buf, &s);
+      assert(chckBufferReadInt16(buf, &s) == 1);
       assert(s == 6);
       assert(chckBufferGetOffset(buf) - chckBufferGetSize(buf) == 0);
       chckBufferFree(buf);
