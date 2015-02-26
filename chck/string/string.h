7848ac3cd58aa2333d8636466fdfb9581b1ad07b
Jari Vetoniemi <mailroxas@gmail.com>
2015-02-26 21:42:05 +0200

    Order functions and add more string functions.

--- chck/string/string.h
+++ chck/string/string.h
@@ -13,16 +13,23 @@ struct chck_string {
 };
 
 static inline bool
-chck_string_eq(const struct chck_string *a, const struct chck_string *b)
+chck_cstr_is_empty(const char *data)
 {
-   return (a->data == b->data) || (a->size == b->size && !memcmp(a->data, b->data, a->size));
+   return (!data || *data == 0);
 }
 
 static inline bool
-chck_string_eq_cstr(const struct chck_string *a, const char *cstr)
+chck_cstr_ends_with(const char *a, const char *b)
 {
-   size_t len = (cstr ? strlen(cstr) : 0);
-   return (len == a->size) && (cstr == a->data || !memcmp(a->data, cstr, a->size));
+   const size_t lena = (a ? strlen(a) : 0), lenb = (b ? strlen(b) : 0);
+   return (lena >= lenb && !memcmp(a + lena - lenb, b, lenb));
+}
+
+static inline bool
+chck_cstr_starts_with(const char *a, const char *b)
+{
+   const size_t lena = (a ? strlen(a) : 0), lenb = (b ? strlen(b) : 0);
+   return (lena >= lenb && !memcmp(a, b, lenb));
 }
 
 static inline bool
@@ -35,6 +42,51 @@ static inline bool
 chck_cstrneq(const char *a, const char *b, size_t len)
 {
    return (a == b) || (a && b && !strncmp(a, b, len));
+}
+
+static inline bool
+chck_string_is_empty(const struct chck_string *string)
+{
+   return chck_cstr_is_empty(string->data);
+}
+
+static inline bool
+chck_string_ends_with_cstr(const struct chck_string *a, const char *cstr)
+{
+   const size_t len = (cstr ? strlen(cstr) : 0);
+   return (a->size >= len && !memcmp(a->data + a->size - len, cstr, len));
+}
+
+static inline bool
+chck_string_starts_with_cstr(const struct chck_string *a, const char *cstr)
+{
+   const size_t len = (cstr ? strlen(cstr) : 0);
+   return (a->size >= len && !memcmp(a->data, cstr, len));
+}
+
+static inline bool
+chck_string_ends_with(const struct chck_string *a, const struct chck_string *b)
+{
+   return (a->size >= b->size && !memcmp(a->data + a->size - b->size, b->data, b->size));
+}
+
+static inline bool
+chck_string_starts_with(const struct chck_string *a, const struct chck_string *b)
+{
+   return (a->size >= b->size && !memcmp(a->data, b->data, b->size));
+}
+
+static inline bool
+chck_string_eq(const struct chck_string *a, const struct chck_string *b)
+{
+   return (a->data == b->data) || (a->size == b->size && !memcmp(a->data, b->data, a->size));
+}
+
+static inline bool
+chck_string_eq_cstr(const struct chck_string *a, const char *cstr)
+{
+   const size_t len = (cstr ? strlen(cstr) : 0);
+   return (len == a->size) && (cstr == a->data || !memcmp(a->data, cstr, a->size));
 }
 
 void chck_string_release(struct chck_string *string);
--- chck/string/test.c
+++ chck/string/test.c
@@ -9,11 +9,20 @@ int main(void)
       struct chck_string str = {0};
       assert(chck_string_set_cstr(&str, "foobar", false));
       assert(str.data && str.size == strlen("foobar") && chck_string_eq_cstr(&str, "foobar"));
+      assert(chck_string_ends_with_cstr(&str, "") && chck_string_ends_with_cstr(&str, NULL) && chck_string_ends_with_cstr(&str, "foobar") && chck_string_ends_with_cstr(&str, "bar") && !chck_string_ends_with_cstr(&str, "bur"));
+      assert(chck_string_starts_with_cstr(&str, "") && chck_string_starts_with_cstr(&str, NULL) && chck_string_starts_with_cstr(&str, "foobar") && chck_string_starts_with_cstr(&str, "foo") && !chck_string_starts_with_cstr(&str, "fuo"));
+      assert(!chck_string_is_empty(&str));
       assert(chck_string_set_cstr(&str, "", false));
       assert(!str.data && str.size == strlen("") && chck_string_eq_cstr(&str, ""));
       assert(!str.data && str.size == 0 && chck_string_eq_cstr(&str, NULL)); // string_eq_cstr treats empty string as NULL
+      assert(chck_string_ends_with_cstr(&str, "") && chck_string_ends_with_cstr(&str, NULL) && !chck_string_ends_with_cstr(&str, "foobar"));
+      assert(chck_string_starts_with_cstr(&str, "") && chck_string_starts_with_cstr(&str, NULL) && !chck_string_starts_with_cstr(&str, "foobar"));
+      assert(chck_string_is_empty(&str));
       assert(chck_string_set_cstr(&str, NULL, false));
       assert(!str.data && str.size == 0 && !chck_string_eq_cstr(&str, "foobar"));
+      assert(chck_string_ends_with_cstr(&str, "") && chck_string_ends_with_cstr(&str, NULL) && !chck_string_ends_with_cstr(&str, "foobar"));
+      assert(chck_string_starts_with_cstr(&str, "") && chck_string_starts_with_cstr(&str, NULL) && !chck_string_starts_with_cstr(&str, "foobar"));
+      assert(chck_string_is_empty(&str));
 
       // is_heap is false, so nothing is copied.
       // since chck_strings do not copy when is_heap is false, printf for .data would print whole "foobar" in this case.
@@ -24,10 +33,16 @@ int main(void)
       struct chck_string str2 = {0};
       assert(chck_string_set(&str2, &str, false));
       assert(str.data == str2.data && str2.size == strlen("foo") && chck_string_eq(&str, &str2));
+      assert(chck_string_ends_with(&str, &str2));
+      assert(chck_string_starts_with(&str, &str2));
       assert(chck_string_set(&str2, &str, true));
       assert(str.data != str2.data && str2.size == strlen("foo") && chck_string_eq(&str, &str2));
+      assert(chck_string_ends_with(&str, &str2));
+      assert(chck_string_starts_with(&str, &str2));
       assert(chck_string_set_cstr(&str2, "foobar2", false));
       assert(str.data != str2.data && str2.size == strlen("foobar2") && !chck_string_eq(&str, &str2));
+      assert(!chck_string_ends_with(&str, &str2));
+      assert(!chck_string_starts_with(&str, &str2));
 
       assert(chck_string_set_format(&str, "%s-%s", "test", str2.data));
       assert(str.data && str.size == strlen("test-foobar2") && str.is_heap && chck_string_eq_cstr(&str, "test-foobar2"));
@@ -47,6 +62,13 @@ int main(void)
       assert(!chck_cstrneq("foobar", "foa", 3));
       assert(chck_cstrneq(NULL, NULL, 3));
       assert(chck_cstrneq(NULL, NULL, 0));
+
+      assert(!chck_cstr_is_empty("foobar"));
+      assert(chck_cstr_ends_with("foobar", "foobar") && chck_cstr_ends_with("foobar", "bar") && !chck_cstr_ends_with("foobar", "bur"));
+      assert(chck_cstr_starts_with("foobar", "foobar") && chck_cstr_starts_with("foobar", "foo") && !chck_cstr_starts_with("foobar", "fur"));
+      assert(chck_cstr_is_empty("") && chck_cstr_is_empty(NULL));
+      assert(chck_cstr_ends_with("", "") && chck_cstr_ends_with(NULL, NULL));
+      assert(chck_cstr_starts_with("", "") && chck_cstr_starts_with(NULL, NULL));
    }
    return EXIT_SUCCESS;
 }
