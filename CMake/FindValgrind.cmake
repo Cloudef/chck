e07911dfa734216558903602c34979ceed21d7ca
Jari Vetoniemi <mailroxas@gmail.com>
2016-02-14 01:33:32 +0200

    CMake: Cleanup FindValgrind

--- CMake/FindValgrind.cmake
+++ CMake/FindValgrind.cmake
@@ -3,15 +3,8 @@
 # VALGRIND_FOUND
 # VALGRIND_INCLUDE_DIRS
 
-find_path(VALGRIND_INCLUDE_DIR NAMES valgrind/valgrind.h valgrind/helgrind.h valgrind/memcheck.h)
+find_path(VALGRIND_INCLUDE_DIRS NAMES valgrind/valgrind.h valgrind/helgrind.h valgrind/memcheck.h)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(valgrind DEFAULT_MSG VALGRIND_INCLUDE_DIR)
-
-if (VALGRIND_FOUND)
-   set(VALGRIND_INCLUDE_DIRS ${VALGRIND_INCLUDE_DIR})
-endif ()
-
-mark_as_advanced(VALGRIND_INCLUDE_DIR)
-
-
+find_package_handle_standard_args(valgrind DEFAULT_MSG VALGRIND_INCLUDE_DIRS)
+mark_as_advanced(VALGRIND_INCLUDE_DIRS)
