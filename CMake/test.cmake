7540edc889306472e9b28c3e84290814959425c3
Jari Vetoniemi <mailroxas@gmail.com>
2016-02-14 15:29:30 +0200

    CMake: Use new API for add_test

--- CMake/test.cmake
+++ CMake/test.cmake
@@ -1,8 +1,8 @@
 # Wraps add_test
-# Fixes win32 .exe extensions on mingw at least
 # Uses CTEST_EXEC_WITH variable which you can set to for example valgrind to run tests with valgrind.
 # Also uses CTEST_OUTPUT_DIRECTORY to set global output directory
 
 function(add_test_ex target)
-   add_test(NAME ${target} COMMAND ${CTEST_EXEC_WITH} "${CTEST_OUTPUT_DIRECTORY}/${target}${CMAKE_EXECUTABLE_SUFFIX}")
+   add_test(NAME ${target} COMMAND ${CMAKE_CROSSCOMPILING_EMULATOR} ${CTEST_EXEC_WITH} $<TARGET_FILE:${target}>)
+   set_target_properties(${target} PROPERTIES RUNTIME_OUTPUT_DIRECTORY "${CTEST_OUTPUT_DIRECTORY}")
 endfunction()
