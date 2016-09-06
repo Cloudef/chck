e8c76d8203cd9865e1a15427948aa5b8697c825d
Jari Vetoniemi <mailroxas@gmail.com>
2016-09-07 00:18:35 +0300

    endianess: Limit generic bswap to sizeof(intmax_t)
    Also do not bswap if size == 1 byte as it makes no sense.

--- chck/buffer/endianess.h
+++ chck/buffer/endianess.h
@@ -70,10 +70,15 @@ chck_endianess chck_endianess(void)
 CHCK_NONULL static inline void
 chck_bswap_generic(void *p, size_t size)
 {
-   size_t s;
-   uint8_t b[size];
+   if (size <= sizeof(uint8_t))
+      return;
+
+   assert(size <= sizeof(intmax_t));
+   uint8_t b[sizeof(intmax_t)];
    memcpy(b, p, size);
-   for (s = 0; s < size; ++s) memset((uint8_t*)p + s, b[size - s - 1], 1);
+
+   for (size_t s = 0; s < size; ++s)
+      memset((uint8_t*)p + s, b[size - s - 1], 1);
 }
 
 CHCK_NONULL static inline void
@@ -105,7 +110,7 @@ chck_bswap(void *v, size_t size, size_t memb)
 {
    assert(v);
 
-   for (uint8_t *p = v; p < (uint8_t*)v + (memb * size); p += size)
+   for (uint8_t *p = v; size > sizeof(uint8_t) && p < (uint8_t*)v + (memb * size); p += size)
       chck_bswap_single(p, size);
 }
 
